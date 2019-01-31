7a3413ff0769e943c09aab54ac9ef95c62b8182e
Jérémy <jeremy.creola@gmail.com>
2019-01-31 14:16:48 +0100

    Initial commit

---
+++ 01-base/02-var-prompt/script.js
@@ -0,0 +1,16 @@
+/* becode/javascript
+ *
+ * /01-base/02-var-prompt/script.js - 1.2: var & prompt
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function name() {
+    let question = prompt("Bonjour bel(le) inconnu(e), puis-je connaître ton prénom ?");
+     alert("Bien le bonjour à toi " + question + " je te trouve magnifique");
+     
+ })();
